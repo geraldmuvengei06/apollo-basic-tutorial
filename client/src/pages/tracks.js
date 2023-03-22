f115f6c2ef0d9f03c0f784e22a76db957fd6c872
Gerald Muvengei <gerald.muvengei@volane.com>
2023-03-22 10:34:27 +0300

    Completed apollo server basic tutorial

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card'
+import { gql, useQuery } from '@apollo/client'
+
+
+const TRACKS = gql`
+  query GetTracks {
+    tracksForHome {
+      title
+      thumbnail
+      length
+      modulesCount
+      id
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`
+
+
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS)
+
+  if (loading) return "Loading..."
+
+  if (error) return `Error!  ${error.message}`
+
+  return <Layout grid> {data?.tracksForHome?.map(track => <TrackCard key={track.id} track={track}></TrackCard> )} </Layout>;
 };
 
 export default Tracks;
